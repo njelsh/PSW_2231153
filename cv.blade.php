05855f4ea43bb87652430188b3aaf4ac21fe5cc1
njelsh <128481355+njelsh@users.noreply.github.com>
2023-03-21 18:54:52 +0700

    Add files via upload
    Website CV menggunakan Laravel

---
+++ cv.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>CV</title>
+    </head>
+    <body>
+        <h1>Anjelica</h1>
+        <h2>3D Designer</h2>
+        <p>
+        I am assertive, outspoken and tend to lead and be determined to achieve my goals. My strength is
+        that I firmly believe in myself. I believe in my thoughts, so I tend to share them with those around
+        me. I am also a strategic mind. I can plan ahead to ensure that my plans are met. This includes
+        resolving crises and tackling challenges that arise along the way. Thus my weakness is my low
+        tolerance level. Especially when alternative ideas are given based on emotional considerations. a
+        fast learner and full of passion. I can work with teams or individuals. It depends on the
+        circumstances of the company.
+        </p>
+        <h3>Skills</h3>
+        <ul>
+            <li>Photoshop</li>
+            <li>Illustrator</li>
+            <li>Animate</li>
+            <li>Public Speaking</li>
+            <li>Blender</li>
+        </ul>
+        <h3>Education</h3>
+        <ul>
+            <li>Bachelor of Information System in Computer Science, International University of Batam, 2024</li>
+        </ul>
+        <h3>Experience</h3>
+        <ul>
+            <li>INTERNSHIP STAFF 2020 (Six Months Internship)
+              </li>
+                <p>PT.Telkomakses Tanjungpinang</p>
+
+                <p>Be part of the warehouse department team that processes input
+                and output hardware transactions such as routers and WiFi
+                extenders and input the data to Telkom's Supply Chain
+                Management application and Excel data according to SOP
+                (Standard Operational Procedure).</p>
+
+            <li>FURNITURE SHOP STAFF Jan 2021 - Mar 202</li>
+                <p>Toko Lingga Furniture</p>
+
+                <p>Guide customers to the best furniture, work with and review new
+                furniture stocked in stores. Advertise and offer new furniture to
+                customers and report daily sales to the owner.</p>
+
+            <li>CASHIER (NIGHT SHIFT) June 2021 - Feb 2022</li>
+                <p>Hotel Winner Pancur Lingga</p>
+
+                <p>I work as a cashier and as a kitchen staff at the same time as a part
+                timer. The workbench helps you serve customers, take orders,
+                inform chefs and coordinate dishes according to customer orders.</p>
+
+
+
+        </ul>
+    </body>
+</html>
